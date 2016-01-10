a12cd4976fbeec7022025ab9b1ddc92e8667a99c
Simon Andrews <simon.andrews212@gmail.com>
2016-01-10 18:58:04 -0500

    Add secret key warning

--- scattergun/scattergun/settings.py
+++ scattergun/scattergun/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@x!oaxv0jf@$oe3&2og2@4!#9^*imqm_!wdre^w^zkxrz$lcbk'
+SECRET_KEY = 'testing'
+if SECRET_KEY = 'testing':
+    print('\033[91mDefault secret key used. This instance is INSECURE!\033[0m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
