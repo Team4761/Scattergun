1b121ee5ed90281e2fe9e6a5ff0bbaa7e772f35d
Simon Andrews <simon.andrews212@gmail.com>
2016-02-27 11:13:01 -0500

    Add team list view + template

--- scattergun/scattergun_coreapp/urls.py
+++ scattergun/scattergun_coreapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^teams/(?P<team_number>\d+)$', views.team_view)
+    url(r'^teams/(?P<team_number>\d+)$', views.team_view),
+    url(r'^teams/', views.team_list_view),
 ]
--- scattergun/scattergun_coreapp/views.py
+++ scattergun/scattergun_coreapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Team
 
 
+def team_list_view(request):
+    teams = Team.objects.all()
+    return render(request, "team_list.html", context={"teams": teams})
+
+
 def team_view(request, team_number):
     team = get_object_or_404(Team, number=team_number)
-    return render(request, "team.html", dictionary={"team": team})
+    return render(request, "team.html", context={"team": team})
