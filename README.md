469db198d9800c37cf4a58f4bc634f221ec6d7a5
Simon Andrews <simon.andrews212@gmail.com>
2016-02-27 11:23:57 -0500

    Add one unit test, travis file, requirements file, and update readme with travis button

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Scattergun
+# Scattergun [![Build Status](https://travis-ci.org/Team4761/Scattergun.svg?branch=master)](https://travis-ci.org/Team4761/Scattergun)
 4761's scouting webapp.
--- scattergun/scattergun_coreapp/tests.py
+++ scattergun/scattergun_coreapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Team
 
-# Create your tests here.
+
+class TeamTestCase(TestCase):
+    def setUp(self):
+        Team.objects.create(name="Test Team 1", number=1)
+
+    def test_team_created_correctly(self):
+        test_team_1 = Team.objects.get(number=1)
+        self.assertEqual(test_team_1.name, "Test Team 1")
+        self.assertEqual(test_team_1.number, 1)
