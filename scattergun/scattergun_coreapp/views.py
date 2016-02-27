e0eb4556eb11b3088251f585045d868308610431
Simon Andrews <simon.andrews212@gmail.com>
2016-02-27 10:49:55 -0500

    View returns 404 if no valid teams found

--- scattergun/scattergun_coreapp/views.py
+++ scattergun/scattergun_coreapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Team
 
+
 def team_view(request, team_number):
-    team = Team.objects.get(number=team_number)
+    team = get_object_or_404(Team, number=team_number)
     return render(request, "team.html", dictionary={"team": team})
