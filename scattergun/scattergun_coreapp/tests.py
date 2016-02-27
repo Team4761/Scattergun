b9c66ca635d48c6d6d04d8b68e7befe910cad347
Simon Andrews <simon.andrews212@gmail.com>
2016-02-27 16:26:17 -0500

    Add unit test for team views

--- scattergun/scattergun_coreapp/tests.py
+++ scattergun/scattergun_coreapp/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
 from .models import Team
 
 
@@ -10,3 +11,12 @@ class TeamTestCase(TestCase):
         test_team_1 = Team.objects.get(number=1)
         self.assertEqual(test_team_1.name, "Test Team 1")
         self.assertEqual(test_team_1.number, 1)
+
+
+class TeamViewTestCase(TestCase):
+    def test_team_views(self):
+        views = ('scattergun-team-add', 'scattergun-team-list')
+        client = Client()
+        for view in views:
+            response = self.client.get(reverse(view))
+            self.assertEqual(response.status_code, 200)
