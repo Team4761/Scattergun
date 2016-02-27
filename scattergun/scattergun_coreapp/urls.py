0f8b21855ccf15f14775b5f6129427132a022679
Simon Andrews <simon.andrews212@gmail.com>
2016-02-27 12:21:30 -0500

    Add form for adding new teams

--- scattergun/scattergun_coreapp/urls.py
+++ scattergun/scattergun_coreapp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^teams/(?P<team_number>\d+)$', views.team_view),
-    url(r'^teams/', views.team_list_view),
+    url(r'teams/add', views.team_add_view, name='scattergun-team-add'),
+    url(r'^teams/(?P<team_number>\d+)$', views.team_view, name='scattergun-team-show'),
+    url(r'^teams/', views.team_list_view, name='scattergun-team-list'),
 ]
--- scattergun/scattergun_coreapp/views.py
+++ scattergun/scattergun_coreapp/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import TeamForm
 from .models import Team
 
 
 def team_list_view(request):
     teams = Team.objects.all()
     return render(request, "team_list.html", context={"teams": teams})
+
+
+def team_add_view(request):
+    if request.method == "POST":
+        form = TeamForm(request.POST)
+        if form.is_valid():
+            team = form.save()
+            return redirect('scattergun-team-show', team_number=team.number)
+    else:
+        form = TeamForm()
+    return render(request, "team_add.html", context={"form": form})
 
 
 def team_view(request, team_number):
