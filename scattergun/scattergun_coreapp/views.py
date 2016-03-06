e993f958805960dcd50c58b9429fa1640df0c379
Simon Andrews <simon.andrews212@gmail.com>
2016-03-06 15:43:17 -0500

    Add views.py from previous commit

--- scattergun/scattergun_coreapp/views.py
+++ scattergun/scattergun_coreapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from .forms import TeamForm, RoundReportForm, CompetitionForm
 from .models import Team, RoundReport, Competition
 
